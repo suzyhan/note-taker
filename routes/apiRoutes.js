8f801b6b50c496dcc5936325e97dda082ea0794a
suzyhan <129558186+suzyhan@users.noreply.github.com>
2023-07-06 02:58:19 -0600

    Create get and post requests for apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,4 +2,23 @@ const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const fs = require ("fs");
 
+// Create GET request for reading and returning retrieved notes as JSON
+router.get('/api/notes', async (req, res) => {
+    const dbJson = await JSON.parse(fs.readFileSync('db/db.json', 'utf-8'));
+    res.json(dbJson);
+});
+
+// Create POST request to receive and save new note
+router.post('/api/notes', (req, res) => {
+    const dbJson = JSON.parse(fs.readFileSync("db/db.json","utf8"));
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uuidv4(),
+    };
+    dbJson.push(newNote);
+    fs.writeFileSync('db/db.json',JSON.stringify(dbJson));
+    res.json(dbJson);
+  });
+
 module.exports = router;
